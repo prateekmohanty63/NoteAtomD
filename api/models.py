c0375d5b00ea6fc186dcf50f00d18c341a895d34
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-24 22:24:48 +0530

    Models setup done

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
+from venv import create
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    body=models.TextField(null=True,blank=True)
+    updated=models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
+
