6fd0306971d42c0de967b05260b09a8d98bf3124
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-24 22:47:22 +0530

    Added rest framework

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {
@@ -37,4 +39,4 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes,safe=False)
+    return Response(routes)
