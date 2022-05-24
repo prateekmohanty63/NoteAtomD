86bfef6da21ddd271bd91008c35ce1c841653579
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-24 23:17:32 +0530

    Feat(Serilizer) Serialized notes

--- api/views.py
+++ api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from .models import Note
+from .serializers import NoteSerializer
 # Create your views here.
 
 @api_view(['GET'])
@@ -40,3 +41,10 @@ def getRoutes(request):
         },
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes=Note.objects.all()
+    serializer=NoteSerializer(notes,many=True)
+    return Response(serializer.data)
